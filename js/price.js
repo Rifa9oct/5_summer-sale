898d268a04bacc96fbf8d882f7f8ab78959b2fa0
Rifa9oct <kohinuraktherr@gmail.com>
2023-08-20 17:00:13 +0600

    reset price sec after click go home btn

--- js/price.js
+++ js/price.js
@@ -4,22 +4,35 @@ function handleClickBtn(data){
     total = parseFloat(total) + parseFloat(price);
     let totalTwoDecimal = total.toFixed(2);
 
+    //reset cart section
     document.getElementById('go-home').addEventListener('click',function(){
-        setInnerText('total-price',0);
-        setInnerText('discount',0);
-        setInnerText('total',0);
+        // remove all prces
+        total = 0;
+        setInnerText('total-price','00 ');
+        setInnerText('discount','00 ');
+        setInnerText('total','00 ');
+
+        // remove li
+        const childs = document.getElementById('li-container').childNodes;
+        document.getElementById('li-container').removeChild(childs[1]);
+        
+        // disable btn
+        document.getElementById('cupon-code').value = '';
+        purchaseBtn.setAttribute('disabled',true);
+        applyBtn.setAttribute('disabled',true);
     })
+
     setInnerText('total-price',totalTwoDecimal);
 
     // enable cupon-code btn
-    const applyBtn =  document.getElementById('apply-btn');
+    let applyBtn =  document.getElementById('apply-btn');
     if(totalTwoDecimal>=200){
         applyBtn.removeAttribute('disabled');
     }
 
     //match cupon-code
     document.getElementById('cupon-code').addEventListener('keyup',function(event){
-        const text = event.target.value
+        const text = event.target.value;
         document.getElementById('apply-btn').addEventListener('click',function(){
             if(text == 'SELL200'){
                  const discount = (totalTwoDecimal* 0.2).toFixed(2);
@@ -31,7 +44,7 @@ function handleClickBtn(data){
     })
 
     // enable purchaseBtn
-    const purchaseBtn = document.getElementById('purchase-btn');
+    let purchaseBtn = document.getElementById('purchase-btn');
     if(totalTwoDecimal>0){
         purchaseBtn.removeAttribute('disabled');
     }
